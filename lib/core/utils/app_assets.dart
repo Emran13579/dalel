edd697bb6ba0d95e80155aea3f92c56ef4e3af39
Emran <alearyemran@gmail.com>
2024-09-30 00:42:04 +0300

    app_colors

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImagesForgetpasswordPng
+  /// assets/images/forgetpassword.png.jpg
+  static const String assetsImagesForgetpasswordPng = "assets/images/forgetpassword.png.jpg";
+
+  /// Assets for assetsImagesOnBording1Png
+  /// assets/images/onBording1.png.png
+  static const String assetsImagesOnBording1Png = "assets/images/onBording1.png.png";
+
+  /// Assets for assetsImagesOnBording2Png
+  /// assets/images/onBording2.png.png
+  static const String assetsImagesOnBording2Png = "assets/images/onBording2.png.png";
+
+  /// Assets for assetsImagesOnBordingPng
+  /// assets/images/onBording.png.png
+  static const String assetsImagesOnBordingPng = "assets/images/onBording.png.png";
+
+  /// Assets for assetsImagesSuccessOrderPng
+  /// assets/images/SuccessOrder.png.png
+  static const String assetsImagesSuccessOrderPng = "assets/images/SuccessOrder.png.png";
+
+  /// Assets for assetsImagesVector1Svg
+  /// assets/images/Vector1.svg.svg
+  static const String assetsImagesVector1Svg = "assets/images/Vector1.svg.svg";
+
+  /// Assets for assetsImagesVector2Svg
+  /// assets/images/Vector2.svg.svg
+  static const String assetsImagesVector2Svg = "assets/images/Vector2.svg.svg";
+}
+
---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors{
+
+static Color primaryColor =const Color(0xffc49E85);
+static Color offwhite =const Color(0xfff8f4f9);
+static Color deepBrown =const Color(0xff6B4B3E);
+static Color grey =Color.fromRGBO(196, 196, 196, 1);
+static Color deebGrey =const Color(0xff6f6460);
+static Color lightGrey=const Color(0xffb4b4b4);
+static Color black =const Color(0xff333333);
+
+}
